10c2976f91c2db2fca6481e54fc681727fb6fbb7
Huawei <iralanda@mail.ru>
2025-06-08 18:49:56 +0300

    Задание 2.2 - Login с использованием Kotlin Flow

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,40 +3,55 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.otus.coroutineshomework.ui.login.data.Credentials
 
 class LoginViewModel : ViewModel() {
 
-    private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
-    val state: LiveData<LoginViewState> = _state
+    private val _stateFlow = MutableStateFlow<LoginViewState>(LoginViewState.Login())
+    val stateFlow: StateFlow<LoginViewState> = _stateFlow.asStateFlow()
     val loginApi = LoginApi()
 
-    fun login(name: String, password: String) {
-        _state.value = LoginViewState.LoggingIn
+    fun login(name: String, password: String){
         viewModelScope.launch {
-            val credentials = Credentials(name, password)
-            try {
-                val user = withContext(Dispatchers.IO) {
-                    loginApi.login(credentials)
-                }
-                _state.value = LoginViewState.Content(user)
-            } catch (e: Exception){
-                _state.value = LoginViewState.Login(e)
+            loginFlow(name, password).collect {
+                _stateFlow.emit(it)
             }
         }
     }
 
-    fun logout() {
-        _state.value = LoginViewState.LoggingOut
+    fun logout(){
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                LoginApi().logout()
+            logoutFlow().collect {
+                _stateFlow.emit(it)
             }
-            _state.value = LoginViewState.Login()
         }
     }
+
+    fun loginFlow(name: String, password: String) = flow<LoginViewState> {
+        emit(LoginViewState.LoggingIn)
+        val credentials = Credentials(name, password)
+        try {
+            val user = loginApi.login(credentials)
+            emit(LoginViewState.Content(user))
+        } catch (e: Exception){
+            emit(LoginViewState.Login(e))
+        }
+    }.flowOn(Dispatchers.IO)
+
+    fun logoutFlow() = flow<LoginViewState> {
+        emit(LoginViewState.LoggingOut)
+        loginApi.logout()
+        emit(LoginViewState.Login())
+    }.flowOn(Dispatchers.IO)
 }
